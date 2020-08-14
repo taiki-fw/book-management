b1149a1076200ec2a55a1fae30e33ec4eb9f05d5
taiki-fw <taiki_52@outlook.jp>
2020-08-14 14:44:07 +0900

    :recycle:バーコードスキャナーのフォルダ構造の変更とローワーキャメルケースの命名に変更

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { BarCodeScannerContainer } from "./src/components/BarcodeScanner";
+import { BarCodeScannerContainer } from "./src/components/BarCodeScanner/BarCodeScanner";
 
 export default function App() {
   return (
@@ -15,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
