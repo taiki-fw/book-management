cd0e91a3b1cef64d5a014c242a342eb946a73c3c
taiki-fw <taiki_52@outlook.jp>
2020-08-17 23:50:06 +0900

    :sparkles:書籍データを保存する機能の追加
    保存ボタンを追加

--- src/components/BarCodeScanner/Book/Book.tsx
+++ src/components/BarCodeScanner/Book/Book.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, Button } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
 
-type Props = {
+type ContainerProps = {
   title: string;
   img: string;
 };
 
-export const Book = (props: Props) => (
+type Props = {
+  storeBook: () => Promise<void>;
+} & ContainerProps;
+
+const Component = (props: Props) => (
   <View>
     <Image style={style.book} source={{ uri: props.img }} />
     <Text>{props.title}</Text>
+    <Button title="保存" onPress={props.storeBook} />
   </View>
 );
 
@@ -19,3 +25,15 @@ const style = StyleSheet.create({
     height: 182,
   },
 });
+
+export const Book = (props: ContainerProps) => {
+  const storeBook = async (book: { title: string; img: string }) => {
+    try {
+      await AsyncStorage.setItem(book.title, JSON.stringify(book));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return <Component {...props} storeBook={() => storeBook(props)} />;
+};
